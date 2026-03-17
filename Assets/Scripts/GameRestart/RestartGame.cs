ea776782eaacbe9cec94316fe5dc6d89e64d2e3f
Tucccc <j5l3t53@gmail.com>
2026-03-17 15:05:57 +0000

    Feat(Script): Reset game
    Added ui for pressing escape so the game can be restarted (re loads the scene)

---
+++ Assets/Scripts/GameRestart/RestartGame.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartGame : MonoBehaviour
+{
+    [SerializeField] private Canvas menu;
+
+    private void Awake()
+    {
+        menu.enabled = false;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) && !menu.enabled)
+        {
+            menu.enabled = true;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && menu.enabled)
+        {
+            menu.enabled = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    public void ResetGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
